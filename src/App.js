8b5e5bb22be818991db5087b5d5aef82955f5917
redelste <redelste@stevens.edu>
2020-07-04 14:01:06 -0700

    Added input validation: no empty inputs

--- src/App.js
+++ src/App.js
@@ -38,29 +38,36 @@ function App() {
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ name: newFriend, age: newAge })
         };
-        fetch('/api/add', requestOptions)
-          .then(response => response.json())
-          .then(data => setFriends({
-            // spread = "..."
-            // it unpacks the dictionary 
-            // must spread a dict into another type of container
 
-            //  what this looks like is:
-            /*
-            {
-              ...{"ham": 303}, "newName": newAge
-            }
+        if (newFriend) {
+          console.log("HERE:LKSDJF:LSKDJF:LSKDJF", newFriend, newAge)
+          fetch('/api/add', requestOptions)
+            .then(response => response.json())
+            .then(data => setFriends({
+              // spread = "..."
+              // it unpacks the dictionary 
+              // must spread a dict into another type of container
 
-            We are taking all values in the original ham dict, and combining it with
-            our new name, age pair into a single dict. 
-            we do this because of immutability.
-            Dont want to mutate things, only create new thingss
-            */
-            ...friends,
-            [`${data.name}`]: data.age
-          })
-          )
-      }}>
+              //  what this looks like is:
+              /*
+              {
+                ...{"ham": 303}, "newName": newAge
+              }
+
+              We are taking all values in the original ham dict, and combining it with
+              our new name, age pair into a single dict. 
+              we do this because of immutability.
+              Dont want to mutate things, only create new thingss
+              */
+              ...friends,
+              [`${data.name}`]: data.age
+            })
+            )
+        } else {
+          alert("Please enter a name")
+        }
+      }
+      }>
         Submit
       </button>
 
@@ -69,4 +76,3 @@ function App() {
 }
 
 export default App;
- 
